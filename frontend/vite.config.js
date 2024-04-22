46affad04598cbcb86b259f6ae3b746402b119a1
sarthakgit21 <110364481+sarthakgit21@users.noreply.github.com>
2024-04-22 14:24:03 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "http://localhost:4000",
+      "/api": "https://hospital-management-site.onrender.com",
     },
   },
   plugins: [react()],
