385e39a74d1ae987176b66d667641e82a66714da
sarthakgit21 <110364481+sarthakgit21@users.noreply.github.com>
2024-04-22 14:23:46 +0530

    Update vite.config.js

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "https://hospital-management-site-1.onrender.com",
+      "/api": "https://hospital-management-site.onrender.com",
     },
   },
   plugins: [react()],
